1e4ce855881d9994a8d665332d2795951a133aca
NobeKanai <qidada4880@gmail.com>
2020-11-24 19:12:50 +0800

    update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,17 +4,23 @@
     :bgImg="bgImg"
     subtitle="Im waiting you for a long time"
   ></my-header>
-  <div class="h-screen"></div>
+  <div class="mx-auto my-4 w-11/12 md:w-9/12 lg:w-1/2 space-y-4">
+    <card>
+      <h1>Nisi enim anim labore dolor eu ad ad sit consequat ea.</h1>
+    </card>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import MyHeader from "@/components/MyHeader.vue";
+import Card from "@/components/Card.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
     MyHeader,
+    Card,
   },
   data() {
     return {
